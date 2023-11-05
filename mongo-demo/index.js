cf71f6d50759de2c9f31fef4637244cc8c444669
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2023-11-05 19:29:11 +0500

    mongodb

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -4,3 +4,48 @@ mongoose
   .connect(`mongodb://localhost/playground`)
   .then(() => console.log(`Connected to Mongodb...`))
   .catch((err) => console.log(`Couldn't connect to Mongodb...`, err));
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Mosh",
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function getCourses() {
+  // eq (equal)
+  // ne (not equal)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
+  const pageNumber = 2;
+  const pageSize = 10;
+
+  // /api/courses?pageNumber=2&pageSize=10
+
+  const courses = await Course.find({ author: "Mosh", isPublished: true })
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  console.log(courses);
+}
+getCourses();
