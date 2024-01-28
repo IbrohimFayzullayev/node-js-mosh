2054b353b0d41c03b2f0300404ebe3d360085616
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2024-01-28 22:56:19 +0500

    movies and rentals added

--- vidly/models/genre.js
+++ vidly/models/genre.js
@@ -1,17 +1,15 @@
 const Joi = require("joi");
 const mongoose = require("mongoose");
 
-const Genre = mongoose.model(
-  "Genre",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+const Genre = mongoose.model("Genre", genreSchema);
 
 function validateGenre(genre) {
   const schema = {
@@ -22,4 +20,5 @@ function validateGenre(genre) {
 }
 
 exports.Genre = Genre;
+exports.genreSchema = genreSchema;
 exports.validate = validateGenre;
