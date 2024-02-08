60504a28c9d707a8efb30afdcf458e22b00976ef
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2024-02-08 23:05:34 +0500

    error handling middleware added

--- vidly/index.js
+++ vidly/index.js
@@ -8,6 +8,7 @@ const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
+const error = require("./middleware/error");
 const express = require("express");
 const app = express();
 
@@ -28,6 +29,7 @@ app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+app.use(error);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
